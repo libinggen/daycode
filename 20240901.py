ba3546ab086597070bd825c625538897fa1cb0b0
libinggen <libinggengit@gmail.com>
2024-09-01 12:48:55 +0900

    reverse list

--- 20240901.py
+++ 20240901.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 
 class Solution:
@@ -38,3 +38,41 @@ class Solution:
                 return [num_map[complement], i]
             num_map[num] = i
         return []
+    
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr:
+            next_temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_temp
+
+        return prev
+    
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None or head.next == None:
+            return head
+        p = None
+        c = head
+        n = head.next
+        while n != None:
+            c.next = p
+            p = c
+            c = n
+            n = n.next
+        c.next = p
+        return c
