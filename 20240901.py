4301a7ce589f4f33651ef06a79d8d79a97d0f7d1
libinggen <libinggengit@gmail.com>
2024-09-02 21:29:34 +0900

    mergeTwoLists

--- 20240901.py
+++ 20240901.py
@@ -1,78 +1,29 @@
-from typing import List, Optional
+from typing import Optional
 
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        l = 0
-        r = len(nums) - 1
-        while l < r:
-            if nums[l] + nums[r] == target:
-                return [l, r]
-            l += 1
-            if l == r:
-                l = 0
-                r -= 1
-                if r == 0:
-                    return []
-        return []
-    
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numMap = {}
-        n = len(nums)
-
-        for i in range(n):
-            complement = target - nums[i]
-            if complement in numMap:
-                return [numMap[complement], i]
-            numMap[nums[i]] = i
-
-        return[]
-    
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
-        return []
-    
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
-        curr = head
-
-        while curr:
-            next_temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_temp
-
-        return prev
     
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head == None or head.next == None:
-            return head
-        p = None
-        c = head
-        n = head.next
-        while n != None:
-            c.next = p
-            p = c
-            c = n
-            n = n.next
-        c.next = p
-        return c
+    def mergeTwoLists(self, list1:Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode
+        current = dummy
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+
+            current = current.next
+
+        if list1:
+            current.next = list1
+
+        if list2:
+            current.next = list2
+
+        return dummy.next
